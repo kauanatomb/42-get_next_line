ede65b7e1e6b29c5f84fc5fac9e5bd9d9a216c31
Kauana Tombolato <kauanatomb@gmail.com>
2025-05-06 16:22:42 +0200

    file bonus header

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktombola <ktombola@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 16:14:27 by ktombola          #+#    #+#             */
+/*   Updated: 2025/05/06 16:18:57 by ktombola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 15
+# endif
+# define MAX_FD 1024
+// libs
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+// utils functions
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+
+#endif
